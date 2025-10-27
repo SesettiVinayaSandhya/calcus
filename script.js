3a9746821f3bcfc730c1e70c422f909852da9a58
SesettiVinayaSandhya <166894920+SesettiVinayaSandhya@users.noreply.github.com>
2025-10-27 11:53:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+    function appendValue(value){
+        document.getElementById("result").value+=value;
+    }
+    function clearResult(){
+        document.getElementById("result").value=" ";
+    }
+    function calculate() {
+        let expression = document.getElementById("result").value;
+        try{
+            document.getElementById("result").value=eval(expression);
+        }
+    
+    catch(error){
+        
+            document.getElementById("result").value="error";
+        }
+    }
+
